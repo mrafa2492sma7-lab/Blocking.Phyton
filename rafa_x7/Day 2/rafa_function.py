62f5d952feea99bde44f98f3b6ea642f5cbab3a1
mrafa2492sma7-lab <m.rafa2492.sma7@sma.prestasiprima.sch.id>
2026-06-05 11:11:23 +0700

    Add files via upload

---
+++ rafa_x7/Day 2/rafa_function.py
@@ -0,0 +1,4 @@
+def my_function():
+    print("oke gas oke gas")
+
+my_function()
---
+++ rafa_x7/Day 2/rafa_polimorfisme.py
@@ -0,0 +1,26 @@
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        print("Drive!")
+
+class Boat:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        print("Sail!")
+
+class Plane:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        print("Fly!")
+car1 = Car("Ford", "Mustang")
+boat1 = Boat("Ibiza", "Touring 20")
+plane1 = Plane("Boeing", "747")
